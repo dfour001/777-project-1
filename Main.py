d195a5559b65eefc21edd36cf19324ab0f5193ad
dfour001 <53315984+dfour001@users.noreply.github.com>
2021-02-20 22:29:41 -0500

    Add ProgressBar widget

--- Main.py
+++ Main.py
@@ -5,8 +5,12 @@
 
 import tkinter as tk
 import tkinter.font as tkFont
+import tkinter.messagebox as messagebox
+from ProgressBar import ProgressBar
 
 root = tk.Tk()
+root.title("GEOG 777 - Project 1 - Daniel Fourquet")
+root.resizable(False, False)
 canvas = tk.Canvas(root, height=600, width=900, bg="red")
 canvas.pack()
 
@@ -16,29 +20,64 @@ canvas.pack()
 # Fonts
 fontTitle = tkFont.Font(family="Lucida Grande", size=20)
 fontParagraph = tkFont.Font(family="Lucida Grande", size=12)
+fontSmall = tkFont.Font(family="Lucida Grande", size=10)
+
+
+def run_analysis(k):
+    """ Runs the geoprocessing tools from RunAnalysis.py, generates
+        output images via ArcGIS Pro, and displays the output in the
+        results frame """
+    
+    # Validate K entry
+    try:
+        k = float(k)
+        if k <= 1:
+            raise ValueError
+    
+    except ValueError:
+        messagebox.showerror("K Value Error", f"K = {k}\n\nInvalid K Value!  K must be a number greater than 1.")
+        return
+
+    # Run analysis
+    btnRunAnalysis["state"] = "disabled"
+
+    # Show progress bar
+    prog = ProgressBar(root)
+    
+    
+
+
+    print('done?')
+
+
+
+
 
 # Setup Frame
-frameSetup = tk.Frame(root, bg="black", bd=10, relief=tk.RIDGE)
-frameSetup.place(relheight=0.75, relwidth=0.35, relx=0.075, rely=0.05)
+frameSetup = tk.Frame(root, bg="black", bd=5, relief=tk.RIDGE)
+frameSetup.place(relheight=0.6, relwidth=0.35, relx=0.075, rely=0.05)
 
 lblTitle = tk.Label(frameSetup,text="Nitrates and Cancer", fg="white", bg="black", justify="center", pady = 1, font=fontTitle)
 lblTitle.place(x=0, y=0, relwidth=1, relheight=0.1)
 
 lblDesc = tk.Label(frameSetup, anchor="n", text=open('projectDescription.txt').read(), fg="white", bg="black", pady = 2, justify="center", wraplength=250, font=fontParagraph)
-lblDesc.place(x=0, rely=0.11, relwidth=1, relheight=0.2)
+lblDesc.place(x=0, rely=0.11, relwidth=1, relheight=0.3)
 
-lblEnterK = tk.Label(frameSetup, text="Enter K value greater than 1 for IDW:", bg="black", fg="white", font=fontParagraph)
-lblEnterK.place(x=0, rely=0.41, relwidth=1)
+lblEnterK = tk.Label(frameSetup, text="Enter K value greater than 1 for IDW:", bg="black", fg="white", font=fontSmall)
+lblEnterK.place(x=0, rely=0.61, relwidth=1)
 
 frameKVal = tk.Frame(frameSetup, bg="black")
-frameKVal.place(x=20, rely=0.47, relwidth=0.8, relheight=0.1)
+frameKVal.place(relx=0.2, rely=0.67, relwidth=0.6, relheight=0.1)
 
-lblKEquals = tk.Label(frameKVal, text="K = ", bg="black", fg="white", font=fontParagraph)
+lblKEquals = tk.Label(frameKVal, text="K = ", bg="black", fg="white", font=fontSmall)
 lblKEquals.pack(side="left")
 
 txtKVal = tk.Entry(frameKVal)
+txtKVal.insert(0, "1.2") # Set default value to 1.2
 txtKVal.pack(side="left")
 
+btnRunAnalysis = tk.Button(frameSetup, text="Run Analysis", command=lambda: run_analysis(txtKVal.get()))
+btnRunAnalysis.place(rely=0.83, relwidth=0.3, relx=0.35)
 
 
 
@@ -47,5 +86,6 @@ frameResults = tk.Frame(root, bg="white")
 frameResults.place(relheight=1, relwidth=0.5, relx=0.5)
 
 
-
+prog = ProgressBar(root)
+prog.set_prog(0.25)
 root.mainloop()
---
+++ ProgressBar.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import Frame, Label
+
+class ProgressBar(Frame):
+    """ A custom widget based on the Frame class that displays a progress bar and status text """
+    def __init__(self, root, initProgress = 0.75):
+        super().__init__(root)
+
+        # Display progress bar frame
+        self.frameProgress = Frame(root, bg="black", bd=5, relief=tk.RIDGE)
+        self.frameProgress.place(relheight=0.2, relwidth=0.5, relx=0.25, rely=0.4)
+        self.frameProgress.focus() # Just to make the entry box lose focus.  The blinking cursor is annoying!
+
+        # Add bar container
+        self.barContainer = Frame(self.frameProgress, bg="white", bd=2, relief=tk.SUNKEN)
+        self.barContainer.place(relheight=0.12, relwidth=0.9, relx=0.05, rely=0.1)
+
+        # Add bar and progress label
+        self.prog = Label(self.barContainer, bg="orange")
+        self.prog.place(x=0, relheight=1, relwidth = float(initProgress))
+        
+        self.lblProg = Label(self.barContainer, bg="black", fg="white", text=str(initProgress))
+        self.lblProg.place(x=0, rely=1, relwidth=1, justify="center")
+
+        # Add status text
+
+
+    def set_prog(self, x):
+        """ Sets the progress bar's value.
+            inputs:
+                x - float between 0 and 1 representing % of progress
+        """
+        self.prog.place(x=0, relheight=1, relwidth=x)
+
+    def set_status(self, text):
+        """ Sets the status label to display input text """
+
+        pass
